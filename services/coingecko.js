78e07a220e3493ca37e53856c1e9e0ef3b990db8
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-08-26 07:34:41 +0000

    add coingecko service

--- services/coingecko.js
+++ services/coingecko.js
@@ -10,8 +10,25 @@ httpClient.defaults.timeout = 2500
 const getLatestPrice = async (p = false) => {
     try {
 
-        const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=dxdao&vs_currencies=usd`)
-        gPrice[p] = response.data['dxdao'].usd
+        if (p && (config[p] || {}).price) {
+            return config[p].price
+        }
+        let baseName = config[p].baseName
+        let quoteName = config[p].quoteName
+        let arr = p.split('-')
+        let baseSymbol = arr[0].toLowerCase()
+        let quoteSymbol = arr[1].toLowerCase()
+        if (quoteSymbol === 'usdt') {
+            quoteSymbol = 'usd'
+        }
+
+        if ( quoteSymbol === 'usd' ) {
+            const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${baseName}&vs_currencies=usd`)
+            gPrice[p] = response.data[baseName].usd
+        } else {
+            const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${baseName},${quoteName}&vs_currencies=usd`)
+            gPrice[p] = (1/response.data[quoteName].usd) * response.data[baseName].usd
+        }
 
     } catch (err) {
         console.log(err)
@@ -28,10 +45,11 @@ const getUSDPrice = async (p = false) => {
 
         let arr = p.split('-')
         baseSymbol = arr[0].toUpperCase()
+        let baseName = config[p].baseName
 
         if (baseSymbol != 'USDT' && baseSymbol != 'USD') {
-            let response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=dxdao&vs_currencies=usd`)
-            let tokenPrice = response.data['dxdao'].usd
+            let response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${baseName}&vs_currencies=usd`)
+            let tokenPrice = response.data[baseName].usd
             gUSDPrice[baseSymbol] = tokenPrice
         } else {
             gUSDPrice[baseSymbol] = 1
